0d3727e23ea12878e2ccc5e4f389d086d73d62df
Halen <911452497@qq.com>
2019-06-10 20:27:10 +0800

    init

---
+++ src/ArraySort.java
@@ -0,0 +1,98 @@
+import java.util.Arrays;
+
+/**
+ * @author Halen
+ * @version 1.0
+ * @date 2019/6/10 19:30
+ */
+public class ArraySort {
+
+    /**
+     * 交换数组指定两个位置的值
+     * @param arr
+     * @param i
+     * @param j
+     */
+    public static void swap(int[] arr, int i, int j) {
+        arr[i] = arr[i] ^ arr[j];
+        arr[j] = arr[i] ^ arr[j];
+        arr[i] = arr[i] ^ arr[j];
+    }
+
+    /**
+     * 使用Arrays.sort()进行排序
+     * @param arr
+     */
+    public static void comparator(int[] arr) {
+        Arrays.sort(arr);
+    }
+
+    /**
+     * 生成最多为maxSize个元素的数组，里面的值最大为maxValue
+     * @param maxSize
+     * @param maxValue
+     * @return
+     */
+    public static int[] generateRandomArray(int maxSize, int maxValue) {
+        int[] arr = new int[(int) ((maxSize + 1) * Math.random())];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int)((maxValue + 1) * Math.random()) - (int) (maxValue * Math.random());
+        }
+        return arr;
+    }
+
+    /**
+     * 复制生成一个新的数组
+     * @param arr
+     * @return
+     */
+    public static int[] copyArray(int[] arr) {
+        if (arr == null) {
+            return null;
+        }
+        int[] res = new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            res[i] = arr[i];
+        }
+        return res;
+    }
+
+    /**
+     * 比较传入的两个数组内容是否一样
+     * @param arr1
+     * @param arr2
+     * @return
+     */
+    public static boolean isEqual(int[] arr1, int[] arr2) {
+        if ((arr1 == null && arr2 != null) || (arr1 != null && arr2 == null)) {
+            return false;
+        }
+        if (arr1 == null && arr2 == null) {
+            return true;
+        }
+        if (arr1.length != arr2.length) {
+            return false;
+        }
+        for (int i = 0; i < arr1.length; i++) {
+            if (arr1[i] != arr2[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * 打印输出给定的数组内容
+     * @param arr
+     */
+    public static void printArray(int[] arr) {
+        if (arr == null) {
+            return;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+}
---
+++ src/BubbleSort.java
@@ -0,0 +1,46 @@
+import java.util.Arrays;
+
+/**
+ * @author Halen
+ * @version 1.0
+ * @date 2019/6/10 19:22
+ * 冒泡排序
+ */
+public class BubbleSort extends ArraySort{
+
+    public static void bubbleSort(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+        for (int i = arr.length - 1; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    swap(arr,j,j+1);
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int testTime = 5000;
+        int maxSize= 100;
+        int maxValue = 100;
+        boolean succeed = true;
+        for (int i = 0; i < testTime; i++) {
+            int[] arr1 = generateRandomArray(maxSize, maxValue);
+            int[] arr2 = copyArray(arr1);
+            bubbleSort(arr1);
+            comparator(arr2);
+            if (!isEqual(arr1, arr2)) {
+                succeed = false;
+                break;
+            }
+        }
+        System.out.println(succeed ? "Good!" : "Fail!");
+        int[] arr = generateRandomArray(maxSize, maxValue);
+        printArray(arr);
+        bubbleSort(arr);
+        printArray(arr);
+    }
+
+}
