6e70d6cb260edc9903b2e2e74a8f1ec500f093ad
Halen <911452497@qq.com>
2019-06-20 23:34:50 +0800

    选择排序

---
+++ src/SelectSort.java
@@ -0,0 +1,54 @@
+/**
+ * @author Halen
+ * @version 1.0
+ * @date 2019/6/20 23:07
+ * 选择排序
+ * 双层循环，时间复杂度O(n^2)
+ */
+public class SelectSort extends ArraySort{
+
+    public static void selectionSort(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+        for (int i = 0; i < arr.length - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                minIndex = arr[j] < arr[minIndex] ? j : minIndex;
+            }
+            swap(arr, i, minIndex);
+        }
+    }
+
+
+    public static void swap(int[] arr, int i, int j) {
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+    }
+
+    public static void main(String[] args) {
+        int testTime = 5000;
+        int maxSize= 100;
+        int maxValue = 100;
+        boolean succeed = true;
+        for (int i = 0; i < testTime; i++) {
+            int[] arr1 = generateRandomArray(maxSize, maxValue);
+            int[] arr2 = copyArray(arr1);
+            selectionSort(arr1);
+            comparator(arr2);
+            if (!isEqual(arr1, arr2)) {
+                printArray(arr1);
+                printArray(arr2);
+                succeed = false;
+                break;
+            }
+        }
+        System.out.println(succeed ? "Good!" : "Fail!");
+        int[] arr = generateRandomArray(maxSize, maxValue);
+        printArray(arr);
+        selectionSort(arr);
+        printArray(arr);
+    }
+
+}
