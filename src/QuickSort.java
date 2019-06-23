cc465b0c354010bf6c57f2ab1365b584609e8c16
Halen <911452497@qq.com>
2019-06-23 23:35:02 +0800

    快速排序

---
+++ src/QuickSort.java
@@ -0,0 +1,68 @@
+/**
+ * @author Halen
+ * @version 1.0
+ * @date 2019/6/23 23:16
+ * 快速排序
+ */
+public class QuickSort extends ArraySort {
+
+    public static void quickSort(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+        quickSort(arr, 0, arr.length - 1);
+    }
+
+    public static void quickSort(int[] arr, int left, int right) {
+        if (left < right) {
+            int partitionIndex = partition(arr, left, right);
+            quickSort(arr, left, partitionIndex - 1);
+            quickSort(arr, partitionIndex + 1, right);
+        }
+    }
+
+    public static int partition(int[] arr, int left, int right) {
+        int pivot = left;
+        int index = pivot + 1;
+        for (int i = index; i <= right; i++) {
+            if (arr[i] < arr[pivot]) {
+                swap(arr, i, index);
+                index++;
+            }
+        }
+        swap(arr, pivot, index - 1);
+        return index - 1;
+    }
+
+    public static void swap(int[] arr, int i, int j) {
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+    }
+
+    public static void main(String[] args) {
+        int testTime = 5000;
+        int maxSize= 100;
+        int maxValue = 100;
+        boolean succeed = true;
+        for (int i = 0; i < testTime; i++) {
+            int[] arr1 = generateRandomArray(maxSize, maxValue);
+            int[] arr2 = copyArray(arr1);
+            quickSort(arr1);
+            comparator(arr2);
+            if (!isEqual(arr1, arr2)) {
+                printArray(arr1);
+                printArray(arr2);
+                succeed = false;
+                break;
+            }
+        }
+        System.out.println(succeed ? "Good!" : "Fail!");
+        int[] arr = generateRandomArray(maxSize, maxValue);
+        printArray(arr);
+        quickSort(arr);
+        printArray(arr);
+    }
+
+
+}
